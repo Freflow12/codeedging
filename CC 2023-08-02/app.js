2a443200ae4471037a9348b044a2edf05b5a328d
Freflow12 <92832955+Freflow12@users.noreply.github.com>
2023-07-03 22:49:59 -0700

    add edge cases from 02/08

--- CC 2023-08-02/app.js
+++ CC 2023-08-02/app.js
@@ -1,9 +1,25 @@
-In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
 
-Examples
-highAndLow("1 2 3 4 5");  // return "5 1"
-highAndLow("1 2 -3 4 5"); // return "5 -3"
-highAndLow("1 9 3 4 -5"); // return "9 -5"
+ // If the function is passed a valid PIN string, return true, else return false.
+
+//Examples (Input --> Output)
+//"1234"   -->  true
+//"12345"  -->  false
+//"a234"   -->  false
+
+function validatePIN (pin) {
+    return /^\d+$/.test(pin) && (pin.length === 4 || pin.length === 6)
+  }
+
+validatePIN([1,2,3,4])
+console.log(validatePIN())
 
 
- 
+
+
+       
+       
+       
+    
+
+    
